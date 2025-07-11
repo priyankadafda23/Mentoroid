d86fd3178a4f96e1e7def1847a43a8b096b796dc
Priyanka Dafda <priyankadafda239@gmail.com>
2025-07-11 19:58:12 +0530

    back

--- BACKEND/leaderboard.py
+++ BACKEND/leaderboard.py
@@ -5,7 +5,7 @@ leaderboard_bp = Blueprint('leaderboard', __name__)
 
 # Helper function to add CORS headers
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
+    response.headers['Access-Control-Allow-Origin'] = 'mentoroid-zeta.vercel.app'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
--- BACKEND/routes/quiz.py
+++ BACKEND/routes/quiz.py
@@ -13,7 +13,7 @@ cors_origin = os.getenv("CORS_ORIGINS")
 
 # Helper function to add CORS headers
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173'
+    response.headers['Access-Control-Allow-Origin'] = 'mentoroid-zeta.vercel.app'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
